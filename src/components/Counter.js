2dad392699bdf9d65f38e27701cc89b8d2e56bf2
Adam Grabarczyk <adamgrabarczyk.gda@wp.pl>
2021-02-07 12:33:45 +0100

    redux setup done

---
+++ src/components/Counter.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {connect} from 'react-redux';
+
+
+import {INCREASE_COUNTER, DECREASE_COUNTER} from '../store/actions/counter';
+
+class Counter extends Component<Props> {
+
+
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={{flexDirection: 'row', width: 200, justifyContent: 'space-around'}}>
+                    <TouchableOpacity onPress={() => this.props.increaseCounter()}>
+                        <Text>Increase</Text>
+                    </TouchableOpacity>
+                    <Text>{this.props.counter.counter}</Text>
+                    <TouchableOpacity onPress={() => this.props.decreaseCounter()}>
+                        <Text>Decrease</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
+}
+
+
+function mapStateToProps(state){
+    return {
+        counter: state.counter
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        increaseCounter: () => dispatch({type: INCREASE_COUNTER}),
+        decreaseCounter: () => dispatch({type: DECREASE_COUNTER})
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter)
+
+const styles = StyleSheet.create({
+    container: {
+        // flex: ,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
---
+++ src/store/reducers/counter.js
@@ -0,0 +1,16 @@
+
+import {DECREASE_COUNTER, INCREASE_COUNTER} from "../actions/counter";
+
+const initialState = {
+    counter: 0
+}
+
+export const reducer = (state = initialState, action) => {
+
+    switch (action.type) {
+        case INCREASE_COUNTER: return{counter: state.counter + 1}
+        case DECREASE_COUNTER: return{counter: state.counter - 1}
+    }
+
+    return state
+}
