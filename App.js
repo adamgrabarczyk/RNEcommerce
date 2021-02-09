9b0349c82b06ffbb6eaf2f20ef4c08e023c3fc1a
Adam Grabarczyk <adamgrabarczyk.gda@wp.pl>
2021-02-09 17:39:36 +0100

    first nav setup

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import {Text, View, SafeAreaView} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 import  Home from './src/screens/Home'
 import  Counter from './src/components/Counter'
 
 const App: () => React$Node = () => {
     return (
+        <NavigationContainer>
         <SafeAreaView>
   <View>
       <Counter/>
+      <Text>dupa</Text>
     </View>
         </SafeAreaView>
+        </NavigationContainer>
   )
 }
 export default App;
