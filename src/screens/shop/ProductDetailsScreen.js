f9117e53711eed21559a41d50f3df95ac72b0006
Adam Grabarczyk <adamgrabarczyk.gda@wp.pl>
2021-03-07 19:41:38 +0100

    add more details to product details screen and style it

--- src/screens/shop/ProductDetailsScreen.js
+++ src/screens/shop/ProductDetailsScreen.js
@@ -5,6 +5,7 @@ import {
 
 } from 'react-native';
 import {useSelector} from 'react-redux';
+import Colors from '../../constans/Colors';
 
 const ProductDetailsScreen = (props, {route, navigation}) => {
 
@@ -13,14 +14,47 @@ const ProductDetailsScreen = (props, {route, navigation}) => {
     const selectedProduct = useSelector(state => state.products.availableProducts.find(prod => prod.id === productId))
 
 return (
-    <View>
-        <Text>{selectedProduct.title}</Text>
-    </View>
+    <ScrollView style={styles.container}>
+        <Image style={styles.image} source={{uri: selectedProduct.imageUrl}}/>
+        <View style={styles.actions}>
+        <TouchableOpacity style={styles.button} onPress={() => {}}><Text style={{color: Colors.primary}}>Add to cart</Text></TouchableOpacity>
+        </View>
+        <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+        <Text style={styles.description}>{selectedProduct.description}</Text>
+    </ScrollView>
 )
 
 };
 
 const styles = StyleSheet.create({
+
+
+    image: {
+            width: '100%',
+            height: 300
+    },
+    actions: {
+        marginVertical: 10,
+        alignItems: 'center'
+
+    },
+
+    button: {
+        alignItems: 'center',
+    },
+
+    price: {
+        fontSize: 20,
+        color: '#888',
+        textAlign: 'center',
+        marginVertical: 20
+    },
+
+    description: {
+        fontSize: 14,
+        textAlign: 'center',
+        marginVertical: 20
+    }
 
 
 
