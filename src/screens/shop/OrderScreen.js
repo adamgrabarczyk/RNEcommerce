4eaefc277f9bcc3095f8c743051d4882849a0850
Adam Grabarczyk <adamgrabarczyk.gda@wp.pl>
2021-03-26 17:59:09 +0100

    create odres screen

---
+++ src/screens/shop/OrderScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text, TouchableOpacity, FlatList, StyleSheet} from 'react-native';
+import {useSelector} from 'react-redux';
+
+const OrderScreen = props => {
+
+    const orders = useSelector(state => state.orders.orders);
+
+    return (
+        <FlatList
+            data={orders}
+            keyExtractor={item => item.id}
+            renderItem={itemData => <Text>{itemData.item.totalAmount}</Text>}
+        />
+    );
+};
+
+
+export default OrderScreen;
