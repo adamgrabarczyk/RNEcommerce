0f255d7ac15f554f08180043b5523ce1f9c3c343
Adam Grabarczyk <adamgrabarczyk.gda@wp.pl>
2021-06-21 19:56:02 +0200

    first modal filters screecn

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {
-    Text, StyleSheet, TouchableOpacity, ScrollView, View, ActivityIndicator, FlatList, Platform, Image
+    Text, StyleSheet, TouchableOpacity, ScrollView, View, Modal, Pressable,  ActivityIndicator, FlatList, Platform, Image
 
 } from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
@@ -10,6 +10,39 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import Colors from '../constans/Colors';
 import * as cartActions from '../store/actions/cart';
 
+const categoryFilters = [
+    {
+        name: 'category_electronic',
+        label: 'Elektronika'
+    },
+    {
+        name: 'category_sport',
+        label: 'Sport'
+    },
+    {
+        name: 'category_car_parts',
+        label: 'Części samochodowe'
+    },
+    {
+        name: 'category_decoration',
+        label: 'Dekoracje'
+    },
+    {
+        name: 'category_clothes',
+        label: 'Odzież'
+    },
+    {
+        name: 'category_garden',
+        label: 'Ogród'
+    },
+    {
+        name: 'category_toys',
+        label: 'Zabawki'
+    }
+
+
+];
+
 
 const Search = (props) => {
     const products = useSelector(state => state.products.availableProducts);
@@ -17,6 +50,8 @@ const Search = (props) => {
     const userFavProducts = useSelector(state => state.products.favoriteUserProducts);
     const newData = useSelector(state => state.products.newData);
     const dispatch = useDispatch();
+
+    const [modalVisible, setModalVisible] = useState(false);
 
     const selectItemHandler = (id, name) => {
         props.navigation.navigate('ProductDetails', {
@@ -29,9 +64,57 @@ const Search = (props) => {
     return (
         <ScrollView>
             <View style={styles.filterButtonContainer}>
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => {
+                        Alert.alert("Modal has been closed.");
+                        setModalVisible(!modalVisible);
+                    }}
+                >
+                    <View style={styles.centeredView}>
+
+                        <View style={styles.modalView}>
+                            <Pressable
+                                style={[styles.button, styles.buttonClose]}
+                                onPress={() => setModalVisible(!modalVisible)}
+                            >
+                                <Text style={styles.textStyle}>X</Text>
+                            </Pressable>
+                            <Text style={styles.modalText}>Filtry</Text>
+                        </View>
+
+                        <View style={styles.modalContent}>
+                            <Text style={styles.modalContentText}>Wybierz kategorie</Text>
+                            <View
+                                style={styles.filtersButtonsArea}
+                            >
+
+                                {
+                                    categoryFilters.map(
+                                        filter => {
+                                            return(
+                                                <TouchableOpacity
+                                                    key={filter.name}
+
+                                                    onPress={() => alert(filter.label)}
+                                                    style={styles.filterButton}
+                                                >
+                                                    <Text style={[styles.filterButton2]}>{filter.label}</Text>
+                                                </TouchableOpacity>
+                                            )}
+                                    )
+                                }
+                            </View>
+                        </View>
+                    </View>
+
+                </Modal>
+
             <TouchableOpacity
                 style={styles.filterButton}
-            onPress={() => alert('pokaz filtry')}
+            onPress={() => setModalVisible(true)}
             >
                 <Ionicons
                     name={Platform.OS === 'android' ? 'options' : 'ios-options'}
@@ -151,6 +234,85 @@ const styles = StyleSheet.create({
     image: {
         width: '100%',
         height: 300
+    },
+
+    centeredView: {
+        flex: 1,
+        flexDirection: 'column',
+        height: '100%',
+        backgroundColor: "white",
+        // padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+    },
+
+    modalView: {
+        flexDirection: 'row',
+        height: 100,
+        width: '100%',
+        backgroundColor: Colors.primary,
+    },
+
+    modalContent: {
+
+    },
+
+    button: {
+        width: 40,
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2
+    },
+    buttonOpen: {
+        backgroundColor: "#F194FF",
+    },
+    buttonClose: {
+        textAlign: 'center',
+        backgroundColor: "#3e8a6f",
+        marginTop: 45,
+        marginLeft: 15,
+        width: 40,
+        height: 40
+    },
+    textStyle: {
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center"
+    },
+    modalText: {
+        marginTop: 55,
+        color: "white",
+        marginBottom: 15,
+        marginLeft: 130,
+        fontSize: 20,
+        fontWeight: '500'
+    },
+    modalContentText: {
+        marginTop: 40,
+        color: "black",
+        fontSize: 20,
+        fontWeight: '500'
+    },
+
+    filterButton2: {
+        color: '#4e5354',
+        margin: 2,
+        fontSize: 18
+    },
+
+    filtersButtonsArea: {
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        textAlign: 'left',
+        marginLeft: -50,
+        marginTop: 50
     }
 
 });
--- src/store/actions/search.js
+++ src/store/actions/search.js
@@ -2,15 +2,8 @@ export const GET_PHRASE = 'GET_PHRASE';
 export const IS_FOCUSED = 'IS_FOCUSED';
 export const IS_NOT_FOCUSED = 'IS_NOT_FOCUSED';
 export const RESET_INPUT = 'RESET_INPUT';
-// export const DELETE_FROM_FAV = 'DELETE_FROM_FAV';
-
 
 export const getPhrase = phrase => {
     return {type: GET_PHRASE, phr: phrase}
 }
 
-// export const deleteFromFav = productId => {
-//     return {type: DELETE_FROM_FAV, pid: productId}
-// }
-//
-//
