ab25aaca8cf00f107ddf1c7df3182eb3baf60a83
Adam Grabarczyk <adamgrabarczyk.gda@wp.pl>
2021-10-03 22:41:57 +0200

    storing oreders to firebase

--- src/store/actions/orders.js
+++ src/store/actions/orders.js
@@ -1,10 +1,41 @@
+
 export const ADD_ORDER = 'ADD_ORDER';
 
 
 export const addOrder = (cartItems, totalAmount) => {
 
-    return {
-        type: ADD_ORDER,
-        orderData: {items: cartItems, amount: totalAmount}
+    return async dispatch => {
+
+        const date = new Date();
+            const response = await fetch(
+                'https://rnecommerce-3bc8a-default-rtdb.europe-west1.firebasedatabase.app/orders/sampleUser.json'
+            , {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    cartItems,
+                    totalAmount,
+                    data: date.toISOString()
+                })
+
+            });
+
+            if (!response.ok) {
+                throw new Error(error);
+            }
+
+            const resData = await response.json();
+
+        dispatch({
+            type: ADD_ORDER,
+            orderData: {
+                id: resData.name,
+                items: cartItems,
+                amount: totalAmount,
+                date: date
+            }
+        })
     }
 }
