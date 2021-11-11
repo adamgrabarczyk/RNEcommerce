ab2fc4844ba32ba587733b7ffb35cbcd2a096166
Adam Grabarczyk <adamgrabarczyk.gda@wp.pl>
2021-11-11 17:02:09 +0100

    store user fav products to firebase

--- src/store/actions/products.js
+++ src/store/actions/products.js
@@ -1,3 +1,5 @@
+import {ADD_ORDER} from './orders';
+
 export const ADD_TO_FAV = 'ADD_TO_FAV';
 export const DELETE_FROM_FAV = 'DELETE_FROM_FAV';
 export const SET_PRODUCTS = 'SET_PRODUCTS';
@@ -23,7 +25,49 @@ export const fetchProducts = () => {
 };
 
 export const addToFav = productId => {
-    return {type: ADD_TO_FAV, pid: productId}
+
+    return async (dispatch, getState) => {
+
+        const favouriteProducts = getState().products.favoriteUserProducts;
+        const products = getState().products.availableProducts;
+        const user = getState().auth.user;
+        console.log(favouriteProducts);
+        let AddNewOrNext;
+
+        if (favouriteProducts.length > 0 && favouriteProducts.filter(product => product)) {
+
+            const newProduct = favouriteProducts.find(product => product.id === productId);
+
+            AddNewOrNext = favouriteProducts.concat(newProduct);
+
+        }else  {
+
+            AddNewOrNext = products.filter(product => product.id === productId);
+        }
+
+        const response = await fetch(
+            `https://rnecommerce-3bc8a-default-rtdb.europe-west1.firebasedatabase.app/fav/${user}.json`
+            , {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    AddNewOrNext
+                })
+
+            });
+
+        if (!response.ok) {
+            throw new Error(error);
+        }
+
+        const resData = await response.json();
+        console.log(resData);
+
+        dispatch({type: ADD_TO_FAV, pid: productId})
+    }
+
 }
 
 export const deleteFromFav = productId => {
