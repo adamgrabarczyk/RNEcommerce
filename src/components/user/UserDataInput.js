fbd3b9a9cbdfaf67665fb6558f80c8a13b4af049
Adam Grabarczyk <adamgrabarczyk.gda@wp.pl>
2021-12-26 19:53:15 +0100

    add update inputs in personal data screen

---
+++ src/components/user/UserDataInput.js
@@ -0,0 +1,49 @@
+import {View, TextInput, StyleSheet, Text} from 'react-native';
+import React from 'react';
+import Colors from '../../constans/Colors';
+
+
+const UserDataInput = (props) => {
+
+
+    return (
+        <View>
+            <View>
+                <Text style={styles.inputName}>{props.inputName}</Text>
+                <TextInput
+                style={styles.inputChange}
+                placeholder={props.inputPalceholder}
+                value={props.inputValue}
+                onChangeText={props.inputChangeText}
+                />
+            </View>
+        </View>
+    );
+}
+
+export default UserDataInput;
+
+
+const styles = StyleSheet.create({
+
+    inputChange: {
+        width: '90%',
+        height: 40,
+        borderColor: Colors.primary,
+        color: '#403e3e',
+        borderWidth: 1,
+        alignItems: 'center',
+        margin: 20,
+        marginTop: 2,
+        fontSize: 18,
+        marginVertical: 5,
+        paddingHorizontal: 5
+    },
+    inputName: {
+        marginLeft: 20,
+        marginTop: 30,
+        fontSize: 15
+    }
+
+});
+
--- src/screens/Profile/PersonalData.js
+++ src/screens/Profile/PersonalData.js
@@ -1,15 +1,93 @@
-import { View, Text, StyleSheet} from 'react-native';
-import React from 'react';
+import {ScrollView, View, Text, StyleSheet, TouchableOpacity, ActivityIndicator} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import UserDataInput from '../../components/user/UserDataInput';
+import Colors from '../../constans/Colors';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
 const PersonalData = () => {
 
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        const getUserData = async () => {
+            const authData = await AsyncStorage.getItem('authData');
+            const parseAuthData = JSON.parse(authData);
+
+            setEmail(parseAuthData.email);
+            setName(parseAuthData.name);
+            setSurname(parseAuthData.surname);
+            setPhone(parseAuthData.phone);
+        }
+        getUserData().then(
+            () => {
+                setLoading(false);
+            }
+        );
+    },[]);
+
+
+    const [name, setName] = useState();
+    const [surname, setSurname] = useState();
+    const [email, setEmail] = useState();
+    const [phone, setPhone] = useState();
+
+    if (loading) {
+        return (
+            <View style={styles.spinnerContainer}>
+                <ActivityIndicator
+                    style={styles.spinner}
+                    size='large'
+                    color={Colors.primary}
+                />
+            </View>
+        )
+    }
+
 
     return (
-        <View>
-            <Text>PersonalData</Text>
+        <ScrollView style={styles.container}>
+            <View style={styles.personalDataHeading}>
+                <Text style={styles.personalDataHeadingText}>Dane konta</Text>
+            </View>
+            <View style={styles.personalDataInputsArea}>
+                <UserDataInput
+                    inputName={'Imię'}
+                    inputPalceholder={'Imię'}
+                    inputValue={name}
+                    inputChangeText={(value) => setName(value)}
+                />
 
-        </View>
+                <UserDataInput
+                    inputName={'Nazwisko'}
+                    inputPalceholder={'Nazwisko'}
+                    inputValue={surname}
+                    inputChangeText={(value) => setSurname(value)}
+                />
+
+                <UserDataInput
+                    inputName={'email'}
+                    inputPalceholder={'email'}
+                    inputValue={email}
+                    inputChangeText={(value) => setEmail(value)}
+                />
+
+                <UserDataInput
+                    inputName={'Telefon'}
+                    inputPalceholder={'Telefon'}
+                    inputValue={phone}
+                    inputChangeText={(value) => setPhone(value)}
+                />
+            </View>
+
+            <TouchableOpacity
+                onPress={() => {}}
+                style={styles.button}
+            >
+                <Text style={styles.buttonText}>Zapisz</Text>
+            </TouchableOpacity>
+        </ScrollView>
     );
 }
 
@@ -17,6 +95,52 @@ export default PersonalData;
 
 
 const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+    },
+
+    personalDataHeading: {
+        alignItems: 'center',
+        padding: 20,
+        borderBottomColor: 'black',
+        borderBottomWidth: 0.6,
+        marginTop: 30,
+        paddingBottom: 30
+    },
+
+    personalDataHeadingText: {
+        color: 'grey',
+        fontSize: 21,
+        fontWeight: 'bold'
+    },
+
+    personalDataInputsArea: {
+        marginTop: 40
+    },
+
+    button: {
+        width: '100%',
+        height: 40,
+        backgroundColor: Colors.primary,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 40
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'normal'
+    },
+
+    spinnerContainer: {
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+
+
 
 
 });
