edae017a599d7110322fd2803cb7a35417064b9a
Adam Grabarczyk <adamgrabarczyk.gda@wp.pl>
2021-12-27 16:00:09 +0100

    install firebase database,first update trial

--- src/store/actions/user.js
+++ src/store/actions/user.js
@@ -1,3 +1,5 @@
+import database from '@react-native-firebase/database';
+
 export const UPDATE_PERSONAL_DATA =  'UPDATE_PERSONAL_DATA';
 export const UPDATE_PASSWORD =  'UPDATE_PASSWORD';
 
@@ -7,6 +9,33 @@ export const updatePersonalData = (id, name, surname, email, phone, key) => {
     console.log(name + ' ' + surname + ' ' + id);
     return async (dispatch) => {
 
+
+        // const userName = await fetch(
+        //     `https://rnecommerce-3bc8a-default-rtdb.europe-west1.firebasedatabase.app/users/${id}/${key}/name.json`
+        //     , {
+        //         method: 'PUT',
+        //         headers: {
+        //             'Content-Type': 'application/json'
+        //         },
+        //         body: JSON.stringify({
+        //             name
+        //         })
+        //     }
+        // );
+        //
+        // if (!userName.ok) {
+        //     throw new Error(error);
+        // }
+
+        database()
+            .ref('/users/'+ id + '/' + key)
+            .update({
+                name: name,
+            })
+            .then(() => console.log('Data updated.'));
+
+
+        console.log('job done');
         dispatch({type: UPDATE_PERSONAL_DATA});
     }
 
