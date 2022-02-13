bca6ef0dfcbc823cf5844335451c05ec08f14085
Adam Grabarczyk <adamgrabarczyk.gda@wp.pl>
2022-02-13 13:39:01 +0100

    basic cinfig ios push notification

--- src/screens/Profile/NotificationsPermission.js
+++ src/screens/Profile/NotificationsPermission.js
@@ -1,6 +1,6 @@
 import { View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import React from 'react';
-import {showNotification, handleCancelNotification, handleShudleNotification} from '../../notification/notification.android';
+import {showNotification, handleCancelNotification, handleShudleNotification} from '../../notification/notification';
 
 
 
