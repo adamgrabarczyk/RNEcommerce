9998b51ca1cf9ea345e3518f496f2cfd5196c8b2
Adam Grabarczyk <adamgrabarczyk.gda@wp.pl>
2022-07-07 07:54:51 +0200

    add simple animation on ellipse elements

--- src/components/UI/Ellipse.js
+++ src/components/UI/Ellipse.js
@@ -1,12 +1,31 @@
-import {StyleSheet, View, Text} from 'react-native';
-import React from 'react';
+import {StyleSheet, View, Animated} from 'react-native';
+import React, {useRef, useEffect} from 'react';
 
 const Ellipse = (props) => {
 
+    const fadeAnim = useRef(new Animated.Value(0)).current;
+
+
+
+    useEffect(() => {
+
+        const fadeIn = () => {
+            Animated.timing(fadeAnim, {
+                toValue: 1,
+                duration: 3000,
+                useNativeDriver: true
+            }).start();
+
+        };
+
+       fadeIn();
+    },[])
 
     return (
-        <View style={[styles.container, props.ellipse]}>
-        </View>
+        <Animated.View style={[styles.container, props.ellipse, {
+            opacity: fadeAnim
+        }]}>
+        </Animated.View>
     )
 
 }
