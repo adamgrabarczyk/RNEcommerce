ff4e25821fd5238c3212e35f952764d976608ee0
Adam Grabarczyk <adamgrabarczyk.gda@wp.pl>
2022-07-31 19:49:41 +0200

    add simple login validation

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -98,88 +98,116 @@ export const signup = (name, surname, phone, email, password) => {
 
 
 export const signin = (email, password) => {
-
+    let error;
     return async (dispatch,getState) => {
-        const prevUserId = getState().cart.user;
-        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCWNcZ_aSsnV-HIWEaqxwz9e0V6zB_jx2w'
-            , {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    email: email,
-                    password: password,
-                    returnSecureToken: true
-                })
+        let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
 
-            });
-
-        if (!response.ok) {
-           const errorResponse = await response.json();
-           const errorMessage = errorResponse.error.message;
-           let error = 'Wprowadź poprawne dane';
-           if (errorMessage === 'EMAIL_NOT_FOUND') {
-               error = 'Wprowadź poprawny adres email';
-           } else if (errorMessage === 'INVALID_PASSWORD') {
-               error = 'Wprowadź poprawne hasło'
-           }
-           throw new Error(error);
+        if (email === '' && password === '') {
+            error = "Wprowadź email i hasło";
+            throw new Error(error);
+        } else if (reg.test(email) === false) {
+            error = "Wprowadź poprawny adres emial";
+            throw new Error(error);
+        } else if (email === '') {
+            error = "Wprowadź adres emial";
+            throw new Error(error);
+        } else if (password === '') {
+            error = "Wprowadź hasło";
+            throw new Error(error);
         }
+        else {
 
-        auth()
-            .signInWithEmailAndPassword(email, password)
-            .then(() => {
-                console.log('User account signed in!');
-            })
-            .catch(error => {
-                if (error.code === 'auth/email-already-in-use') {
-                    console.log('That email address is already in use!');
+            const prevUserId = getState().cart.user;
+            const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCWNcZ_aSsnV-HIWEaqxwz9e0V6zB_jx2w'
+                , {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        email: email,
+                        password: password,
+                        returnSecureToken: true
+                    })
+
+                });
+
+            if (!response.ok) {
+                const errorResponse = await response.json();
+                const errorMessage = errorResponse.error.message;
+                error = 'Wprowadź poprawne dane';
+                if (errorMessage === 'EMAIL_NOT_FOUND') {
+                    error = 'Wprowadź poprawny adres email';
+                } else if (errorMessage === 'INVALID_PASSWORD') {
+                    error = 'Wprowadź poprawne hasło'
                 }
-
-                if (error.code === 'auth/invalid-email') {
-                    console.log('That email address is invalid!');
-                }
-
-                console.error(error);
-            });
-
-        const resData = await response.json();
-
-        const userDataResponse = await fetch(
-            `https://rnecommerce-3bc8a-default-rtdb.europe-west1.firebasedatabase.app/users/${resData.localId}.json`
-        );
-
-        const userResData = await userDataResponse.json();
-
-        const personalUserData = {
-            name: '',
-            surname: '',
-            phone: '',
-            avatar: '',
-            key: ''
-
-        };
-
-        for (const key in userResData) {
-            if (userResData[key].userId === resData.localId)  {
-                personalUserData.name = userResData[key].name;
-                personalUserData.surname = userResData[key].surname;
-                personalUserData.phone = userResData[key].phone;
-                personalUserData.avatar = userResData[key].avatar.uri;
-                personalUserData.avatarPath = userResData[key].avatar.imageUri;
-                personalUserData.key = key;
+                throw new Error(error);
             }
-        }
-        if (resData.localId !== prevUserId)  {
-            dispatch({ type: 'RESET_USER_LOG' })
-        }
 
-        dispatch({type: LOGIN, token: resData.idToken, email: email, user: resData.localId, name: personalUserData.name, surname: personalUserData.surname,
-            phone: personalUserData.phone, avatar: personalUserData.avatar, avatarPath: personalUserData.avatarPath, key: personalUserData.key });
-        const expireDate = new Date(new Date().getTime() + parseInt(resData.expiresIn) * 1000);
-        setDataToStorage(resData.idToken, resData.localId, expireDate, email, personalUserData.name, personalUserData.surname,
-            personalUserData.phone, personalUserData.avatar, personalUserData.key);
+            auth()
+                .signInWithEmailAndPassword(email, password)
+                .then(() => {
+                    console.log('User account signed in!');
+                })
+                .catch(error => {
+                    if (error.code === 'auth/email-already-in-use') {
+                        console.log('That email address is already in use!');
+                    }
+
+                    if (error.code === 'auth/invalid-email') {
+                        console.log('That email address is invalid!');
+                    }
+
+                    console.error(error);
+                });
+
+            const resData = await response.json();
+
+            const userDataResponse = await fetch(
+                `https://rnecommerce-3bc8a-default-rtdb.europe-west1.firebasedatabase.app/users/${resData.localId}.json`
+            );
+
+            const userResData = await userDataResponse.json();
+
+            const personalUserData = {
+                name: '',
+                surname: '',
+                phone: '',
+                avatar: '',
+                key: ''
+
+            };
+
+            for (const key in userResData) {
+                if (userResData[key].userId === resData.localId) {
+                    personalUserData.name = userResData[key].name;
+                    personalUserData.surname = userResData[key].surname;
+                    personalUserData.phone = userResData[key].phone;
+                    personalUserData.avatar = userResData[key].avatar.uri;
+                    personalUserData.avatarPath = userResData[key].avatar.imageUri;
+                    personalUserData.key = key;
+                }
+            }
+            if (resData.localId !== prevUserId) {
+                dispatch({type: 'RESET_USER_LOG'})
+            }
+
+            dispatch({
+                type: LOGIN,
+                token: resData.idToken,
+                email: email,
+                user: resData.localId,
+                name: personalUserData.name,
+                surname: personalUserData.surname,
+                phone: personalUserData.phone,
+                avatar: personalUserData.avatar,
+                avatarPath: personalUserData.avatarPath,
+                key: personalUserData.key
+            });
+            const expireDate = new Date(new Date().getTime() + parseInt(resData.expiresIn) * 1000);
+            setDataToStorage(resData.idToken, resData.localId, expireDate, email, personalUserData.name, personalUserData.surname,
+                personalUserData.phone, personalUserData.avatar, personalUserData.key);
+        }
     }
 }
 
